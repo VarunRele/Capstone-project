02d6f0658146ed04ed49deb769366935148a0424
unknown <rele.varun1@gmail.com>
2023-02-03 18:36:07 +0530

    Authentication added

--- restaurant/views.py
+++ restaurant/views.py
@@ -23,4 +23,4 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    # permission_classes = [IsAuthenticated] 
+    permission_classes = [IsAuthenticated] 
